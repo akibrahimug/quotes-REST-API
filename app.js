29be03ccc35192d86f2b788b67799e7e34178860
Kasoma Ibrahim <kasomaibrahim@gmail.com>
2022-04-04 20:51:38 +0100

    creating modules

--- app.js
+++ app.js
@@ -1,53 +1,24 @@
 const express = require('express');
-const { restart } = require('nodemon');
 const app = express();
-const records = require('./records')
+const routes = require('./routes')
 
-app.use(express.json())
-// Send a GET request /quotes to READ all the quotes
-app.get('/quotes', async(req,res) => {
-    try{
-        const quotes = await records.getQuotes();
-        res.json(quotes)
-    }catch(err) {
-        res.json({message: err.message})
-    }
-    
-})
-// Send a GET request /qoutes/:id to READ a quote
-app.get('/quotes/:id', async(req,res) => {
-    try{
-        const quote = await records.getQuote(req.params.id);
-        if(qoutes){
-            res.json(quote)
-        }else{
-            res.status(404).json({message: "Quote not found"})
+app.use(express.json());
+app.use('/api', routes)
+
+
+// Global Error Handler Middle Ware
+app.use((req, res, next) => {
+    const err = new Error("Not Found");
+    err.status = 404;
+    next(err)
+});
+
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.json({
+        error: {
+            message: err.message
         }
-    }catch(err){
-        res.status(500).json({message: err.message})
-    }
-
+    })
 })
-// Send a POST request /qoutes to CREATE a quote
-app.post('/quotes', async(req,res) => {
-    try{
-        if(req.body.author && req.body.quote){
-            const createQuote = await records.createQuote({
-                quote: req.body.quote,
-                author: req.body.author
-            })
-            res.status(201).json(createQuote)
-        }else{
-            res.status(400).json({message: "Quote and Author required"})
-        }
-        
-    }catch(err){
-        res.status(500).json({message: err.message})
-    }
-})
-// Send a PUT request /quotes/:id toUPDATE a quote
-// Send a DELETE request /quotes/:id to DELETE a quote
-
-// Send a GET request /quotes/quote/random to READ a random quote
-
 app.listen(3000, () => console.log('Quote API listening on port 3000!'));
